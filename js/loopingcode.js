bc54a2e201d44fc50bc4c894ca8b0fda228e3c27
Hardeep <deshardeep@gmail.com>
2023-12-06 12:14:08 +0530

    using loopingcode in js

---
+++ js/loopingcode.js
@@ -0,0 +1,28 @@
+// For loop
+// for(let i =1; i<=5; i++)
+// {
+//     console.log('iteration number' + i)
+// }
+
+// While loop
+
+// let i = 1
+// while(i<=5){
+//     console.log('iteration number' + i)
+//     i++
+// }
+
+// do while loop
+// let i = 6
+// do{
+//     console.log('iteration number' + i)
+//     i++
+// }while(i <= 5)
+
+
+// for of loop
+
+const numGroup = [1,2,3,4,5]
+for(const num of numGroup){
+    console.log('itration number' + num)
+}

b2436b271b8594c0239c0d1dd74b4138d560b77e
Hardeep <deshardeep@gmail.com>
2023-12-06 11:39:49 +0530

    Using Typeconversions  in js

---
+++ js/Typeconversions.js
@@ -0,0 +1,27 @@
+// console.log(true + '3')
+// console.log('4' - '2')
+// console.log('4' * '2')
+// console.log('4' / '2')
+// console.log('bruce' - 'Mayne')
+// console.log('5'- true)
+// console.log('5'- false)
+// console.log('5'- null)
+// console.log(5 + undefined)
+
+// Examples of typeconversions
+
+console.log(Number('5'))
+console.log(Number('false'))
+console.log(Number(''))
+console.log(parseInt('5'))
+console.log(parseFloat('3.14'))
+
+// Covert Other Datatypes to String
+console.log(String(500))
+console.log(String(true))
+console.log(String(null))
+console.log(String(undefined))
+console.log((500).toString())
+
+// Convert Other Datatypes to Boolean
+console.log(Boolean(10)) // if we use null, undefined, 0, '', Null . all return false. everything else will return true

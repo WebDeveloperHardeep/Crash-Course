b57a4ab8fd0780fa5428f42d8ade60ada6f3c269
Hardeep <deshardeep@gmail.com>
2023-12-06 11:22:35 +0530

    Using Operatos in js

---
+++ js/Operators.js
@@ -0,0 +1,43 @@
+let x = 10 //// Assingment Opertors
+let y = 5
+
+// Arthumatic Operators
+console.log(x + y)
+console.log(x - y)
+console.log(x * y)
+console.log(x / y)
+console.log(x % y)
+console.log(++x)
+console.log(--y)
+
+// Comparsion Operators
+console.log(x == y)
+console.log(x != y)
+console.log(x === y)
+console.log(x !== y)
+console.log(x >= y)
+console.log(x > y)
+console.log(x <= y)
+console.log(x < y)
+
+// Logical Operators
+// AND Opeartor
+const isValidNumber = x > 8 && 8 > y
+console.log(isValidNumber)
+// OR Opeator
+const isValidNumber1 = x > 8 || 8 > y
+console.log(isValidNumber1)
+
+// logical not operator
+const isValid =true
+console.log(isValid)
+
+// String Operators 
+// Using Plus opratos to join two or more strings
+
+console.log('bruce' + 'Wayne') // Concatnation
+
+// Ternary Opeartor
+
+const isEven = 10 % 2 === 0 ? 'Number is Even' : 'Number is odd'
+console.log(isEven)

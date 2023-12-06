2ae2aaa87cea011b3a388189639b5c15c2208eb9
Hardeep <deshardeep@gmail.com>
2023-12-06 12:00:22 +0530

    Using Conditional Statement in js

---
+++ js/conditionalstatement.js
@@ -0,0 +1,24 @@
+const num = 0
+if(num > 0){
+    console.log('Number is Positive')
+}else if (num < 0){
+    console.log('Number is not Negtive')
+}else{
+    console.log('Number is Zero')
+}
+
+const color = 'red'
+
+switch(color){
+    case 'red':
+    console.log('Color is red')
+    break;
+    case 'blue':
+    console.log('Color is blue')
+    break;            
+    case 'green':
+    console.log('Color is green')
+    break; 
+    default:
+    console.log('Not a Valid Colorf')  
+}

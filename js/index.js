22c6c541cce1a9c3837cb011fcb3f79eb757ab81
Hardeep <deshardeep@gmail.com>
2023-12-06 11:05:50 +0530

    Using Variables & Data Types in js

---
+++ js/index.js
@@ -0,0 +1,49 @@
+// Log a statement using console log()
+// console.log("Hello From Index.js")
+
+// Variables
+
+// let age = 25;
+// age = 29;
+// console.log(age)
+
+// const salary = 8000;
+// salary = 80000;
+// console.log(salary)
+
+// let sum = 0;
+// sum = 5
+// console.log(sum)
+
+// Data Types
+
+const name = 'vishwas'
+const language = 'Javascript'
+const channel = 'Codeevoluation'
+const total = 0
+const P1 = 3.14
+
+const isPrimaryNumber = true
+const isNewUser = false
+let result
+console.log(result)
+
+const res = null // no user this instead null
+const data = null
+// ================================================
+
+const person ={
+    'firstName':'Bruce',
+    'lasttName':'Kayne',
+    'age':'24',
+}
+
+console.log(person.firstName)
+
+const oddNumbers = [1,3,5,7,9]
+console.log(oddNumbers[2])
+
+let a = 10
+a = 'Vishwass'
+a = true
+console.log(a)

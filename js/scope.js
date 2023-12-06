1f61ba2cc2c47b6622b112fc989aab8f9a45dde5
Hardeep <deshardeep@gmail.com>
2023-12-06 12:30:31 +0530

    Using Scope in js

---
+++ js/scope.js
@@ -0,0 +1,17 @@
+const  myNum = 100
+const myName1 ='Babloo'
+if(true){
+    const myName = 'Hardeep'
+    console.log(myName)
+    console.log(myName1)
+    console.log(myNum)
+}
+
+function testFn(){
+    const myName = 'Gurdeep'
+    console.log(myName)
+    console.log(myName1)
+    console.log(myNum)
+
+}
+testFn()
